96c5ffbd56afd0aad8aa5aac0229a770f17ca0d8
yunjjung <yjung203@naver.com>
2022-12-25 12:29:32 +0900

    [ADD]로그아웃, 메인 Linear로 변경

--- app/src/main/java/com/example/kidsapp/MainActivity.java
+++ app/src/main/java/com/example/kidsapp/MainActivity.java
@@ -5,14 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth;
+
     private BottomNavigationView bottomNavigationView; //하단탭
     private FragmentManager fm;
     private FragmentTransaction ft;
@@ -83,6 +90,19 @@ public class MainActivity extends AppCompatActivity {
                 break;
 
         }
+
+        Button btnLogout = findViewById(R.id.btn_logout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //로그아웃
+                mFirebaseAuth.signOut();
+
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
     }
 }
