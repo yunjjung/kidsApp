96ef320d9e382a5805432d1f0707dd7a94c952f9
yunjjung <yjung203@naver.com>
2023-06-30 14:35:55 +0900

    [ADD]3축 데이터 클래스

--- app/src/main/java/com/example/kidsapp/ThSensorData.java
+++ app/src/main/java/com/example/kidsapp/ThSensorData.java
@@ -1,6 +1,41 @@
 package com.example.kidsapp;
-
+//x,y,z 축이 있는 센서 데이터 들을 위한 클래스
 public class ThSensorData {
     private float X;
+    private float y;
+    private float z;
+
+    public ThSensorData(){};
+
+    public ThSensorData(float x, float y, float z){
+
+    }
+
+    public float getX() {
+        return X;
+    }
+
+    public void setX(float x) {
+        X = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+
+
+
 
 }
